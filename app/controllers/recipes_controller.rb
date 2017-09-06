b6e3e577a706fbd07f5d92200bad743bfafcfa0a
xthegoodguy <xashinx@gmail.com>
2017-09-05 21:26:35 -0500

    what we learned in class

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -14,6 +14,46 @@ class RecipesController < ApplicationController
                         ingredients: params[:ingredients],
                         directions: params[:directions]
                         )
-    # recipe.save
+     recipe.save
+     flash[:success] = "Recipe Successfully Created"
+     redirect_to "/recipes/#{recipe.id}"
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def update
+    recipe = Recipe.find(params[:id])
+
+    recipe.assign_attributes(
+                        title: params[:title],
+                        chef: params[:chef],
+                        ingredients: params[:ingredients],
+                        directions: params[:directions]
+                        )
+
+    #recipe.update does assign_attributes and save
+
+    # recipe.title = params[:title]
+    # recipe.chef = params[:chef]
+    # recipe.ingredients = params[:ingredients]
+    # recipe.directions = params[:directions]
+    recipe.save
+    flash[:success] = "Successfully updated"
+    redirect_to "/recipes/#{ recipe.id }"
+  end
+  def delete
+    @recipe = Recipe.find(params[:id])
+
+    @recipe.destory
+    flash[:success] = "Successfully deleted"
+    redirect_to '/recipes'
   end
 end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
   get '/recipes/new' => 'recipes#new'
   post '/recipes' => 'recipes#create'
   #even though dulpicate url, it's fine because different verb
-  get 'recipes/:id' => 'recipes#show'
+  get '/recipes/:id' => 'recipes#show'
+  get '/recipes/:id/edit' => 'recipes#edit'
+  patch '/recipes/:id' =>'recipes#update'
+
+  delete '/recipes/:id' => 'recipes#delete'
 end
